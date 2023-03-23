9ee2d58f9fb114db2832eba9fbbcb0680c8b790e
AndriiKot <a_kocyuba@ukr.net>
2023-03-23 19:20:40 +0100

    add switch and if

---
+++ demo.js
@@ -0,0 +1,41 @@
+﻿let i = 5;
+
+  if (i == 0){
+    console.log(0);
+  } else if (i == 1){
+    console.log(1);
+  } else if (i == 2){
+    console.log(2)
+  } else if (i == 3){
+    console.log(3)
+  } else if (i == 4){
+    console.log(4)
+  } else if (i == 5){
+    console.log(5)
+  } else {
+    console.log("X")
+  }
+  
+
+let color = "blu"
+
+  switch(color){
+    case 'red':
+      console.log('red');
+      break;
+    case "yellow":
+      console.log('yellow');
+      break;
+    case "black":
+      console.log('black');
+      break;
+    case "green":
+      console.log('green');
+      breack;
+    case "blue":
+      console.log('blue');
+      break;
+    default:
+      console.log("I do not specified color");
+    break;
+  }
