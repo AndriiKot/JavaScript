1b2ce186f2e2b58f8ab2751bcb7f9b5f3aefa054
AndriiKot <a_kocyuba@ukr.net>
2023-03-23 18:24:15 +0100

    Creation lesson 3-2

---
+++ main.js
@@ -0,0 +1,24 @@
+﻿let defaultName = "Anonim"
+
+function sayHI(name) {
+  if(name == null){
+    alert("HI, " + defaultName + "!");
+  } else {
+    alert("Hi, " +  name + "!");
+  }
+}
+
+function printVars(a,b,c,d){
+  console.log(a,b,c,d)
+}
+
+let var1,Var2,$var3,_var4
+
+var1 = 1; Var2 = 2; $var3 = 3; _var4 = 4
+
+printVars(var1,Var2,$var3,_var4)  // 1 2 3 4 
+
+var1 = 4; Var2 = 3; $var3 = 2; _var4 = 1
+
+printVars(var1,Var2,$var3,_var4)
+
