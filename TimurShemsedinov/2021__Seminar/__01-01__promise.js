a5b8d8cfa7264d5fc625b59ed9b8db0ee99f2b68
AndriiKot <a_kocyuba@ukr.net>
2024-10-05 08:23:19 +0200

    ECMAScript 'Timur Shemsedinov' '2021 Seminar' ! prettier !

--- TimurShemsedinov/2021__Seminar/__01-01__promise.js
+++ TimurShemsedinov/2021__Seminar/__01-01__promise.js
@@ -1,14 +1,14 @@
-'use strict'
+"use strict";
 
 const fn = (a, b) => {
   return new Promise((res, rej) => {
-  	if (typeof a !== 'number') rej(new Error('Number'));
-  	if (typeof b !== 'number') rej(new Error('Number'));
-  	res(a + b);
+    if (typeof a !== "number") rej(new Error("Number"));
+    if (typeof b !== "number") rej(new Error("Number"));
+    res(a + b);
   });
 };
 
 (async () => {
-	const res = await fn(1, 2);
-	console.log({ res });	
+  const res = await fn(1, 2);
+  console.log({ res });
 })();
--- TimurShemsedinov/2021__Seminar/__01-02__promise-error.js
+++ TimurShemsedinov/2021__Seminar/__01-02__promise-error.js
@@ -1,18 +1,18 @@
-'use strict'
+"use strict";
 
 const fn = (a, b) => {
   return new Promise((res, rej) => {
-  	if (typeof a !== 'number') rej(new Error('Number'));
-  	if (typeof b !== 'number') rej(new Error('Number'));
-  	res(a + b);
+    if (typeof a !== "number") rej(new Error("Number"));
+    if (typeof b !== "number") rej(new Error("Number"));
+    res(a + b);
   });
 };
 
 (async () => {
   try {
-  	const res = await fn(1, '2'); // error arg
-		console.log({ res });
-	} catch (err) {
-		console.log(err.stack);
-	}	
+    const res = await fn(1, "2"); // error arg
+    console.log({ res });
+  } catch (err) {
+    console.log(err.stack);
+  }
 })();
