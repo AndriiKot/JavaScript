381549617add597eb2792ba2eceb0a65c2e64b92
AndriiKot <a_kocyuba@ukr.net>
2024-10-28 10:17:14 +0100

    ECMAScript 'Timur Shemsedinov' '2024 Course' ! Array and functions !

---
+++ TimurShemsedinov/2024__Course/JS_beginner/__01__calculateTotal.js
@@ -0,0 +1,12 @@
+﻿
+const calculateTotal = (prices) => {
+  let amount = 0;
+  if (!Array.isArray(prices)) throw new Error(`Array expected`)
+  for(const price of prices) {
+    amount += price;
+  }
+  return amount;
+}
+
+console.log(calculateTotal([1, 2, 3]));
+console.log(calculateTotal(1, 2, 3));
